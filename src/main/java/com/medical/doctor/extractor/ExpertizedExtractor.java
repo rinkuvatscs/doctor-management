1d3e1564ec4863a27e053cd134040102da99b25e
Aviral Mittal <aviralmittal2012@gmail.com>
2017-03-21 18:22:32 +0530

    bugs fix

--- src/main/java/com/medical/doctor/extractor/ExpertizedExtractor.java
+++ src/main/java/com/medical/doctor/extractor/ExpertizedExtractor.java
@@ -7,19 +7,20 @@ import java.util.Map;
 
 import org.springframework.jdbc.core.ResultSetExtractor;
 
-public class ExpertizedExtractor implements ResultSetExtractor<Map<Integer, String>> {
+public class ExpertizedExtractor implements
+		ResultSetExtractor<Map<Integer, String>> {
 
-    @Override
-    public Map<Integer, String> extractData(ResultSet rs) throws SQLException {
+	@Override
+	public Map<Integer, String> extractData(ResultSet rs) throws SQLException {
 
-        Map<Integer, String> expertizedMap = new HashMap<>();
-        while (rs.next()) {
+		Map<Integer, String> expertizedMap = new HashMap<>();
+		while (rs.next()) {
 
-            expertizedMap.put(rs.getInt("id"), rs.getString("expertise"));
-        }
-System.out.println(expertizedMap.size());
-        return expertizedMap;
+			expertizedMap.put(rs.getInt("expertiseId"), rs.getString("expertise"));
+		}
+		System.out.println(expertizedMap.size());
+		return expertizedMap;
 
-    }
+	}
 
 }
