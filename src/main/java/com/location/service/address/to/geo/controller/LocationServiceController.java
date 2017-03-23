0f9fa8400eb15979aa845d6b0048ea60697819ca
Rinku <rinkuvatscs@gmail.com>
2017-03-24 01:08:26 +0530

    Location Service changes

---
+++ src/main/java/com/location/service/address/to/geo/controller/LocationServiceController.java
@@ -0,0 +1,29 @@
+package com.location.service.address.to.geo.controller;
+
+import io.swagger.annotations.Api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.location.service.address.to.geo.utility.LocationUtility;
+import com.location.service.address.to.geo.utility.response.LocationResponse;
+
+@RestController
+@RequestMapping("/api/location")
+@Api(basePath = "/location", value = "location", description = "Operations with Landlords", produces = "application/json")
+public class LocationServiceController {
+
+	@Autowired
+	private LocationUtility locationUtility;
+
+	@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, value = "/{address}", method = RequestMethod.GET)
+	@ResponseBody
+	public LocationResponse getLocationGeoCode(@PathVariable String address) {
+		return locationUtility.getGeoCodeFromAddress(address);
+	}
+}
--- src/main/java/com/medical/DoctorManagementApplication.java
+++ src/main/java/com/medical/DoctorManagementApplication.java
@@ -2,8 +2,10 @@ package com.medical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.medical" , "com.location"})
 public class DoctorManagementApplication {
 
 	public static void main(String[] args) {
