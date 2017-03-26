80a8848d9aabb5e198b0ade7a2aea1ac6bfb0a77
Mahima-95 <agg.mahima@gmail.com>
2017-03-26 21:10:53 +0530

    contact fix. addContact & getContact tested and getAll Contact & advertiseDoctor commented...

--- src/main/java/com/medical/doctor/controller/MiscController.java
+++ src/main/java/com/medical/doctor/controller/MiscController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,7 +27,7 @@ import com.medical.doctor.factory.MiscFactory;
 import com.medical.doctor.response.Response;
 
 @RestController
-@RequestMapping("/misc")
+@RequestMapping("/api/misc")
 @Api(basePath = "/misc", value = "misc", description = "Operations with Landlords", produces = "application/json")
 public class MiscController {
 
@@ -36,34 +37,29 @@ public class MiscController {
 	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, value = "/singleto", method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.CREATED)
 	@ApiOperation(value = "add new email", notes = "add new email")
-	@ApiResponses(value = {
-			@ApiResponse(code = 400, message = "Fields are with validation errors"),
+	@ApiResponses(value = { @ApiResponse(code = 400, message = "Fields are with validation errors"),
 			@ApiResponse(code = 201, message = "") })
-	public Response sendEmail_SingleTO(
-			@RequestBody com.medical.doctor.request.MiscEmailRequest miscEmailRequest) {
+	public Response sendEmail_SingleTO(@RequestBody com.medical.doctor.request.MiscEmailRequest miscEmailRequest) {
 
 		Email email = new Email();
 		try {
 			BeanUtils.copyProperties(miscEmailRequest, email);
 		} catch (BeansException beansException) {
-			throw new BadRequestException("Email address is not correct",
-					beansException);
+			throw new BadRequestException("Email address is not correct", beansException);
 		}
 
 		if (!StringUtils.isEmpty(email) && !StringUtils.isEmpty(email.getTo())
 				&& !StringUtils.isEmpty(email.getMessage())) {
 			return new Response(miscFactory.getMiscService().sendMail(email));
 		} else {
-			throw new BadRequestException(
-					"Receiver's address and message can't be blank");
+			throw new BadRequestException("Receiver's address and message can't be blank");
 		}
 	}
 
 	@RequestMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE, value = "/singletomultiplecc", method = RequestMethod.POST)
 	@ResponseStatus(HttpStatus.CREATED)
 	@ApiOperation(value = "add new email", notes = "add new email")
-	@ApiResponses(value = {
-			@ApiResponse(code = 400, message = "Fields are with validation errors"),
+	@ApiResponses(value = { @ApiResponse(code = 400, message = "Fields are with validation errors"),
 			@ApiResponse(code = 201, message = "") })
 	public Response emailSingleTO_MultipleCC(
 			@RequestBody com.medical.doctor.request.MiscEmailRequest miscEmailRequest) {
@@ -72,17 +68,14 @@ public class MiscController {
 		try {
 			BeanUtils.copyProperties(miscEmailRequest, email);
 		} catch (BeansException beansException) {
-			throw new BadRequestException("Email address is not correct",
-					beansException);
+			throw new BadRequestException("Email address is not correct", beansException);
 		}
 
 		if (!StringUtils.isEmpty(email) && !StringUtils.isEmpty(email.getTo())
 				&& !StringUtils.isEmpty(email.getMessage())) {
-			return new Response(miscFactory.getMiscService()
-					.SingleTo_MultipleCC(email));
+			return new Response(miscFactory.getMiscService().SingleTo_MultipleCC(email));
 		} else {
-			throw new BadRequestException(
-					"Receiver's address and message can't be blank");
+			throw new BadRequestException("Receiver's address and message can't be blank");
 		}
 	}
 
@@ -94,16 +87,16 @@ public class MiscController {
 		return response;
 	}
 
-	@RequestMapping(value = "/getContact", method = RequestMethod.GET)
-	public Contact getContact() {
-		return miscFactory.getMiscService().getContact();
+	@RequestMapping(value = "/getContact/{email}/email", method = RequestMethod.GET)
+	public Contact getContact(@PathVariable String email) {
+		return miscFactory.getMiscService().getContact(email);
 	}
 
-	@RequestMapping(value = "/getAll", method = RequestMethod.GET)
-	public List<Contact> getAll() {
-
-		return miscFactory.getMiscService().getAll();
-	}
+	/*
+	 * @RequestMapping(value = "/getAll", method = RequestMethod.GET) public
+	 * List<Contact> getAll(@RequestBody Contact contact) { return
+	 * miscFactory.getMiscService().getAll(contact); }
+	 */
 
 	@RequestMapping(value = "/updateContact", method = RequestMethod.PUT)
 	public String updateContent(@RequestBody Contact contact) {
@@ -113,16 +106,15 @@ public class MiscController {
 
 	// controller for advertise doctor in front page of UI
 
-	@RequestMapping("/advertiseDoctor")
-	public List<AdvertiseDoctor> test() {
-		AdvertiseDoctor advertiseDoctor = new AdvertiseDoctor();
-		advertiseDoctor.setId(1);
-		advertiseDoctor.setDoctorName("Mahima");
-		advertiseDoctor.setClinicName("ABC");
-		advertiseDoctor.setClinicAddress("Delhi");
-		List<AdvertiseDoctor> response = miscFactory.getMiscService()
-				.getDoctorByDate(advertiseDoctor);
-		return response;
-
-	}
+	/*
+	 * @RequestMapping("/advertiseDoctor") public List<AdvertiseDoctor> test() {
+	 * AdvertiseDoctor advertiseDoctor = new AdvertiseDoctor();
+	 * advertiseDoctor.setId(1); advertiseDoctor.setDoctorName("Mahima");
+	 * advertiseDoctor.setClinicName("ABC");
+	 * advertiseDoctor.setClinicAddress("Delhi"); List<AdvertiseDoctor> response
+	 * = miscFactory.getMiscService() .getDoctorByDate(advertiseDoctor); return
+	 * response;
+	 * 
+	 * }
+	 */
 }
--- src/main/java/com/medical/doctor/service/impl/MiscUsingRestCallImpl.java
+++ src/main/java/com/medical/doctor/service/impl/MiscUsingRestCallImpl.java
@@ -17,13 +17,12 @@ import com.medical.doctor.service.MiscService;
 @Service
 public class MiscUsingRestCallImpl implements MiscService {
 
-	
 	@Autowired
 	private ContactDao contactDao;
-	
+
 	@Autowired
 	private AdvertiseDoctorDao advertiseDoctorDao;
-	
+
 	@Override
 	public String sendMail(Email email) {
 		// TODO Auto-generated method stub
@@ -51,8 +50,8 @@ public class MiscUsingRestCallImpl implements MiscService {
 	}
 
 	@Override
-	public Contact getContact() {
-		return contactDao.getContact();
+	public Contact getContact(String email) {
+		return contactDao.getContact(email);
 	}
 
 	@Override
@@ -61,8 +60,8 @@ public class MiscUsingRestCallImpl implements MiscService {
 	}
 
 	@Override
-	public List<Contact> getAll() {
-		return contactDao.getAll();
+	public List<Contact> getAll(Contact contact) {
+		return contactDao.getAll(contact);
 	}
 
 	@Override
